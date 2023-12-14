b7bb72bb7455fb97018f864e49f6abc4d2a5855a
ronatkumar6084 <142104691+ronatkumar6084@users.noreply.github.com>
2023-12-14 11:06:08 +0530

    Update switchcase.java

--- switchcase.java
+++ switchcase.java
@@ -14,16 +14,16 @@ public class switchcase {
             System.out.println("You are ready to join school");
             break;
             case 14:
-            System.out.println("Your hoghschool journey begins");
+            System.out.println("Your highschool journey begins");
             break;
             case 16:
-            System.out.println("Your intermediate journet begins");
+            System.out.println("Your intermediate journey begins");
             break;
             case 18:
-            System.out.println("Your Geraduation journey begins");
+            System.out.println("Your Graduation journey begins");
             break;
             case 23:
-            System.out.println("You shouls look for a job");
+            System.out.println("You should look for a job");
             break;
             default:
             System.out.println("Enter valid age");
